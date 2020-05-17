5512791d0134ce4b6ae90991a2fccf6d3c768d12
vunguyen <vunguyen475@gmail.com>
2020-05-17 22:26:43 +0700

    update source

---
+++ wp-content/themes/shopfifa/page-templates/home.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: Home Page
+ */
+get_header();
+?>
+    <!--Start Pull HTML here-->
+    <main id="main-content" class="main">
+        <h1>oko kokokokok  home page</h1>
+    </main>
+    <!--END  Pull HTML here-->
+<?php get_footer();
+
